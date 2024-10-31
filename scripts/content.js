c17b771e7287571e219bcb3061de0f1e059b523f
mardomai <mardo.mai@ametikool.ee>
2024-10-31 23:29:47 +0200

    yesa

--- scripts/content.js
+++ scripts/content.js
@@ -1,22 +1,28 @@
-const article = document.querySelector("article");
+console.log("Content script loaded on delfi.ee");
 
-// `document.querySelector` may return null if the selector doesn't match anything.
+const article = document.querySelector(".c-body-text.c-main-content");
+
 if (article) {
+  console.log("Article element found");
   const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const wordMatchRegExp = /[^\s]+/g; // Regular expression 
   const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
   const wordCount = [...words].length;
   const readingTime = Math.round(wordCount / 200);
   const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
   badge.classList.add("color-secondary-text", "type--caption");
   badge.textContent = `⏱️ ${readingTime} min read`;
 
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
+  const heading = article.querySelector("h2");
   const date = article.querySelector("time")?.parentNode;
 
-  (date ?? heading).insertAdjacentElement("afterend", badge);
+  if (date || heading) {
+    (date ?? heading).insertAdjacentElement("afterend", badge);
+    console.log("Badge inserted");
+  } else {
+    article.insertAdjacentElement("afterbegin", badge);
+    console.log("Neither date nor heading element found. Badge inserted at the beginning of the article.");
+  }
+} else {
+  console.log("Article element not found");
 }
